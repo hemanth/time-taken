fd8fbc68db6cda03ba5de863bd8871656428d7b4
hemanth <hemanth.hm@gmail.com>
2015-03-18 15:04:14 +0000

    Better isFunc.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 var now = require("performance-now");
 
 module.exports = function (func) {
-	if( func && {}.toString.call(func) === '[object Function]') {
+	if( func && typeof func === 'function') {
        var t0 = now();
        func();
        var t1 = now();
        return t1 - t0;
+    } else {
+        throw new Error("Expecting a function.");
     }
 };
